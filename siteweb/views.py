47b4aa788c76c3aa754f1de18525e09aa15004c6
Tano Martin <78958657+Tano-Martin@users.noreply.github.com>
2022-02-08 14:26:21 +0000

    Update views.py

--- siteweb/views.py
+++ siteweb/views.py
@@ -77,8 +77,8 @@ def contactPost(request):
             )
             contact.save()
             if created:
-                message = f"Nom et Prénoms : {nom}\nEmail : {email}\nSujet : {sujet}\n\n{message}"
-                send_mail(sujet, message, email, [settings.EMAIL_HOST_USER], fail_silently=False)
+                # message = f"Nom et Prénoms : {nom}\nEmail : {email}\nSujet : {sujet}\n\n{message}"
+                # send_mail(sujet, message, email, [settings.EMAIL_HOST_USER], fail_silently=False)
                 test_message = "Votre message a bien été envoyé !"
             else:
                 test_message = "Votre message est déjà envoyé !"
